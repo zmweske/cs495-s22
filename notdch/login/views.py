aab3bdf21b4f14627cc01099c90c4641a0973d0b
Cole Robbins <crobbins3@crimson.ua.edu>
2022-03-01 14:16:30 -0600

    Basic Patient login functionality

---
+++ notdch/login/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import LoginForm
+from .models import Patient
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        
+        if form.is_valid():
+            user = Patient.objects.get(username=form.cleaned_data['username'])
+            
+            if user.password == form.cleaned_data['password']:
+                return render(request, 'success.html')
+                #return HttpResponseRedirect('/success/')
+            
+            else:
+                return render(request, 'failure.html')
+                #return HttpResponseRedirect('/failure/')
+            
+        
+    else:
+        form = LoginForm()
+        
+    return render(request, 'login.html', {'form': form})
+
+def home(request):
+    return render(request, 'home.html')
+
+def success(request):
+    return render(request, 'success.html')
+
+def failure(request):
+    return render(request, 'failure.html')
