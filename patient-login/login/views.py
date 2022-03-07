dc6753dac7ce96969071dc903d419f540fd9b77c
Jake-Cloud <jlcloud@crimson.ua.edu>
2022-03-06 21:17:42 -0600

    Starting work on doctor search and payment pages, began adding django template to make frontend editing easier

--- patient-login/login/views.py
+++ patient-login/login/views.py
@@ -8,6 +8,9 @@ from .forms import LoginForm
 from .models import Patient
 
 def login(request):
+    errorMsg = None
+    record = None
+
     if request.method == 'POST':
         form = LoginForm(request.POST)
         
@@ -21,13 +24,19 @@ def login(request):
                 cursor.execute(sqlStatment)
                 record = cursor.fetchone()    
             except:
-                return render(request, 'login.html', {'error_message': 'ERROR- Check your MySQL Syntax before continuing', 'form': form})
-            if record == None:
-                return render(request, 'login.html', {'error_message': 'Incorrect username or password!', 'form': form})
-            
-            username = record[0]
-            user = Patient.objects.get(username=username)
-            return render(request, 'success.html', context={"user" : username, "date" : user.patient_since})
+                #return render(request, 'login.html', {'error_message': 'ERROR- Check your MySQL Syntax before continuing', 'form': form})
+                errorMsg = 'ERROR- Check your MySQL Syntax before continuing'
+                
+            if record == None and errorMsg == None:
+                #return render(request, 'login.html', {'error_message': 'Incorrect username or password!', 'form': form})
+                errorMsg = 'Incorrect username or password!'
+
+            if errorMsg == None:
+                username = record[0]
+                user = Patient.objects.get(username=username)
+                return render(request, 'success.html', context={"user" : username, "date" : user.patient_since})
+
+            return render(request, 'login.html', {'error_message': errorMsg, 'form': form, 'navbar': 'login'})
             # user = Patient.objects.get(username=form.cleaned_data['username'])
             
             # if user.password == form.cleaned_data['password']:
@@ -42,13 +51,19 @@ def login(request):
     else:
         form = LoginForm()
         
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'login.html', {'form': form, 'navbar': 'login'})
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'navbar': 'home'})
 
 def success(request):
     return render(request, 'success.html')
 
 def failure(request):
     return render(request, 'failure.html')
+
+def doctor(request):
+    return render(request, 'doctor.html', {'navbar': 'doctor'})
+
+def payment(request):
+    return render(request, 'payment.html', {'navbar': 'payment'})
