0cb8137c0ce27f0df01d74e4a74b09b1faaaf40c
Jake-Cloud <jlcloud@crimson.ua.edu>
2022-03-24 13:54:58 -0500

    Changed file structure

--- patient-login/notdch/settings.py
+++ patient-login/notdch/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for notdch project.
 
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'notdch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
