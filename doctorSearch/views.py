ccca34c1c9f7951ed17d5d5afc0b88e2cfa8ab57
Jake-Cloud <jlcloud@crimson.ua.edu>
2022-04-07 13:38:38 -0500

    Fixed

--- doctorSearch/views.py
+++ doctorSearch/views.py
@@ -15,8 +15,8 @@ def search(request):
             name = form.cleaned_data['name'].rsplit(' ')
             
             #Will search for either name in db if space exists
-            # first_name = name[0] # method_1
-            # last_name = name[0] if len(name) < 2 else name[1] # method_1
+            first_name = name[0]
+            last_name = name[0] if len(name) < 2 else name[1]
             
             # #match_queryset = Doctor.objects.filter(first_name__contains=form.cleaned_data['name']) # method_1
             match_queryset = Doctor.objects.filter(Q(first_name__icontains=first_name) | Q(first_name__icontains=last_name) | Q(last_name__icontains=first_name) | Q(last_name__icontains=last_name))
